01715c623ae83a2f8d4cc739031b694f94acc6ca
semanticker <semanticker@gmail.com>
2023-04-12 21:14:38 +0900

    #8 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    * 스프링 웹
    * 엑츄에이터 
    * 데브툴즈
    * 롬복

---
+++ src/main/java/kr/semanticker/projectboard/FastCampusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.semanticker.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/semanticker/projectboard/FastCampusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.semanticker.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
