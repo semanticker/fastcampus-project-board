b011912b2541ecd3d64fec44140fbe76d60762ce
semanticker <semanticker@gmail.com>
2023-04-20 07:36:39 +0900

    #15 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 입력되는 데이터 이고
    도메인과 직접적인 연관이 없으므로 추출가능
    '@MappedSupperClass'를 이용해서 상속하는 방식으로 추출함.
    '@DateTimeFormat' 요소를 추가해서 날짜 포멧을 지정함.

--- src/main/java/kr/semanticker/projectboard/domain/Article.java
+++ src/main/java/kr/semanticker/projectboard/domain/Article.java
@@ -4,12 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Cascade;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
@@ -24,9 +18,9 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class Article {
+public class Article extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -41,12 +35,6 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
     protected Article() {}
 
