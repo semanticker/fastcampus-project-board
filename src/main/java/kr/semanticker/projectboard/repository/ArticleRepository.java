957be57a0218f67470619451f0d54c1c0e5ae61a
semanticker <semanticker@gmail.com>
2023-04-21 07:24:10 +0900

    #1 - @RepositoryRestResource 추가
    data rest 생성을 위해서 어노테이션 추가

--- src/main/java/kr/semanticker/projectboard/repository/ArticleRepository.java
+++ src/main/java/kr/semanticker/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package kr.semanticker.projectboard.repository;
 
 import kr.semanticker.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
