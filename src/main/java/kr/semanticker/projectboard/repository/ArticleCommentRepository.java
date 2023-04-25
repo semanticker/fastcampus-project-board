11e8d696da092962f8fd8e6ba047dca4538e5f7d
semanticker <semanticker@gmail.com>
2023-04-26 07:38:17 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    'QuerydslPredicateExecutor',
    'QuerydslBinderCustomer'를 이용하여 간단하게 개발

--- src/main/java/kr/semanticker/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/kr/semanticker/projectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,32 @@
 package kr.semanticker.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import kr.semanticker.projectboard.domain.Article;
 import kr.semanticker.projectboard.domain.ArticleComment;
+import kr.semanticker.projectboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+
+        // 선택적 검색이 가능하도록 변경
+        // 모든 검색 키워드 활성화를 비활성화
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt, root.createdBy);
+
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // like '%${v}%'
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+
+    }
 }
--- src/main/java/kr/semanticker/projectboard/repository/ArticleRepository.java
+++ src/main/java/kr/semanticker/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,44 @@
 package kr.semanticker.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import kr.semanticker.projectboard.domain.Article;
+import kr.semanticker.projectboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.time.DateTimeException;
+
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        // 선택적 검색이 가능하도록 변경
+        // 모든 검색 키워드 활성화를 비활성화
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        // 이그젝트 매치
+        // bindings.bind(root.title).first(((path, value) -> path.eq(value)));
+        // 이그젝트 매치2
+        //bindings.bind(root.title).first(SimpleExpression::eq);
+
+        // like 검색
+        //bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // like '%${v}%'
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+
+
+    }
 }
