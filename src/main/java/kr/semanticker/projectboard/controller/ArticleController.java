da95220275e04f0180951f839b36be87c7e0940e
semanticker <semanticker@gmail.com>
2023-04-27 15:24:24 +0900

    #19 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 view를 열어줄지 테스트로 명세를 작성

---
+++ src/main/java/kr/semanticker/projectboard/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package kr.semanticker.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+}
