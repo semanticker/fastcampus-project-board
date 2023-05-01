a1ab026bf1ac22ff5083cb37f3ed89e238b19a2a
semanticker <semanticker@gmail.com>
2023-05-01 15:34:11 +0900

    #19 - Thymeleaf 설정
    decoupled 설정을 사용하기 위한 라이브러리 추가
    설정 클래스(ThymeleafConfig) 추가
    설정으로 decoupled 작동하도록 application.yml 설정

--- src/main/java/kr/semanticker/projectboard/config/ThymeleafConfig.java
+++ src/main/java/kr/semanticker/projectboard/config/ThymeleafConfig.java
@@ -1,10 +1,11 @@
 package kr.semanticker.projectboard.config;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
 public class ThymeleafConfig {
@@ -20,7 +21,8 @@ public class ThymeleafConfig {
     }
 
 
-    @ConstructorBinding
+    @RequiredArgsConstructor
+    @Getter
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
         /**
@@ -28,13 +30,6 @@ public class ThymeleafConfig {
          */
         private final boolean decoupledLogic;
 
-        public Thymeleaf3Properties(boolean decoupledLogic) {
-            this.decoupledLogic = decoupledLogic;
-        }
-
-        public boolean isDecoupledLogic() {
-            return this.decoupledLogic;
-        }
-    }
+     }
 
 }
