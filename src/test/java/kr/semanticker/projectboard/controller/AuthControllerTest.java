e388a6d59b3d0c35dc59f2aec334a78775e0e15c
semanticker <semanticker@gmail.com>
2023-05-04 06:54:01 +0900

    #19 - 로그인 컨트롤러 테스트 에러 수정
    AuthControllerTest.java 에 생성자 파라메터를 @Autowired 하도록 처리

--- src/test/java/kr/semanticker/projectboard/controller/AuthControllerTest.java
+++ src/test/java/kr/semanticker/projectboard/controller/AuthControllerTest.java
@@ -3,6 +3,7 @@ package kr.semanticker.projectboard.controller;
 import kr.semanticker.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -18,7 +19,7 @@ public class AuthControllerTest {
 
     private final MockMvc mvc;
 
-    public AuthControllerTest(MockMvc mvc) {
+    public AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
