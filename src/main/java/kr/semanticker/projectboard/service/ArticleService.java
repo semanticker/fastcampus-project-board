b9df1ecd743d6eb8b12c52d2c487f2adaedcfcba
semanticker <semanticker@gmail.com>
2023-05-05 22:41:53 +0900

    #21 - 게시글 테스트 스켈레톤 작성
    게시글 테스트를 위한 스켈레톤 코드 생성

---
+++ src/main/java/kr/semanticker/projectboard/service/ArticleService.java
@@ -0,0 +1,7 @@
+package kr.semanticker.projectboard.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+}
---
+++ src/test/java/kr/semanticker/projectboard/service/ArticleServiceTest.java
@@ -0,0 +1,32 @@
+package kr.semanticker.projectboard.service;
+
+import kr.semanticker.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("비지니스 로직 - 게시글")
+@RequiredArgsConstructor
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceTest {
+
+    @InjectMocks private ArticleService sut;
+
+    @Mock private ArticleRepository articleRepository;
+
+    @DisplayName("게시글을 검색하면, 게시글 리스트를 반환한다.")
+    @Test
+    void givenSearchParameters_whenSearchingArticles_thenReturnsArticleLisg() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
