22ca60895bae0b75f0fd0a3cfe591ac50b0dfbc2
semanticker <semanticker@gmail.com>
2023-05-13 09:09:53 +0900

    #21 - Article, ArticleComment 조인 컬럼 변경
    -> user_account_id

--- src/main/java/kr/semanticker/projectboard/domain/ArticleComment.java
+++ src/main/java/kr/semanticker/projectboard/domain/ArticleComment.java
@@ -8,7 +8,7 @@ import lombok.ToString;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table( indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -20,18 +20,21 @@ public class ArticleComment extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+    @Setter @ManyToOne(optional = false) @JoinColumn(name = "user_account_id") private UserAccount userAccount;
     @Setter @Column(nullable = false, length = 500) private String content; // 본
 
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
