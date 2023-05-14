a038f25968e809c6ca67bbfce38960f19b21f9e2
semanticker <semanticker@gmail.com>
2023-05-14 22:12:13 +0900

    #21 - 루트 경로를게시판 페이지로 리다이렉트 하는 로직 테스트 추가

--- src/test/java/kr/semanticker/projectboard/controller/MainControllerTest.java
+++ src/test/java/kr/semanticker/projectboard/controller/MainControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -18,6 +19,15 @@ class MainControllerTest {
 
     public MainControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
+    }
+
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
