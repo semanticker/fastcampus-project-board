c412bc7d76b4bcc1aeedf89185a840f56a0e6d27
semanticker <semanticker@gmail.com>
2023-06-06 22:17:13 +0900

    #34-해시태그만 검색하기 위해서 커스텀 Repository 생성

---
+++ src/main/java/kr/semanticker/projectboard/repository/querydsl/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,27 @@
+package kr.semanticker.projectboard.repository.querydsl;
+
+import com.querydsl.jpa.JPQLQuery;
+import kr.semanticker.projectboard.domain.QArticle;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class ArticleRepositoryCustomImpl extends QuerydslRepositorySupport implements ArticleRepositoryCustom {
+
+    public ArticleRepositoryCustomImpl(Class<?> domainClass) {
+        super(domainClass);
+    }
+
+    @Override
+    public List<String> findAllDistinctHashtags() {
+
+        QArticle article = QArticle.article;
+
+        JPQLQuery<String> query = from(article)
+                .distinct()
+                .select(article.hashtag)
+                .where(article.hashtag.isNotNull());
+
+        return query.fetch();
+    }
+}
