9fe6f7d48d6db61dc44277859682ec9b85c0ef78
semanticker <semanticker@gmail.com>
2023-06-10 21:29:23 +0900

    #34-해시태그 검색을 위해서 커스텀 Respository 호환 하능하게 하기

--- src/main/java/kr/semanticker/projectboard/repository/ArticleRepository.java
+++ src/main/java/kr/semanticker/projectboard/repository/ArticleRepository.java
@@ -4,6 +4,7 @@ import kr.semanticker.projectboard.domain.Article;
 import kr.semanticker.projectboard.domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import kr.semanticker.projectboard.repository.querydsl.ArticleRepositoryCustom;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,6 +16,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
+        ArticleRepositoryCustom,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
