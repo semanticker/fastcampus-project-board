9459800970068862b3fce6d29506e5c5a45553ae
semanticker <semanticker@gmail.com>
2023-06-12 22:26:14 +0900

    #34 - 테스트 실패 디버깅
    같은 흐름이지만 예약된 문자열과 실제 실행 결과 문자열이 공백 하나의 차이로 다르다고 판별됨.
    해당 코드를 일치 시킴

--- src/main/java/kr/semanticker/projectboard/service/ArticleService.java
+++ src/main/java/kr/semanticker/projectboard/service/ArticleService.java
@@ -43,7 +43,7 @@ public class ArticleService {
     public ArticleWithCommentsDto getArticle(Long articleId) {
         return articleRepository.findById(articleId)
                 .map(ArticleWithCommentsDto::from)
-                .orElseThrow(()-> new EntityNotFoundException("게시글이 없습니다 - articleId:" + articleId));
+                .orElseThrow(()-> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
         //return null;
     }
 
