4ee95a3b260759523d1528d1fef562e5796eabe7
semanticker <semanticker@gmail.com>
2023-07-01 22:20:47 +0900

    #41-댓글을 위한 ArticleCommentController 구현
    컨트롤러 구현
    ArticleCommentRequest 추가

---
+++ src/main/java/kr/semanticker/projectboard/dto/request/ArticleCommentRequest.java
@@ -0,0 +1,20 @@
+package kr.semanticker.projectboard.dto.request;
+
+import kr.semanticker.projectboard.dto.ArticleCommentDto;
+import kr.semanticker.projectboard.dto.UserAccountDto;
+
+public record ArticleCommentRequest(Long articleId, String content) {
+
+    public static ArticleCommentRequest of(Long articleId, String content) {
+        return new ArticleCommentRequest(articleId, content);
+    }
+
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
+        return ArticleCommentDto.of(
+                articleId,
+                userAccountDto,
+                content
+        );
+    }
+
+}
