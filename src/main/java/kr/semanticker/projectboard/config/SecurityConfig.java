81d30bd22d62c8b9a0192d3e866916e725ff1e36
semanticker <semanticker@gmail.com>
2023-07-07 23:31:09 +0900

    #43-SpringSecurity 기본 설정중
    mvcMatchers(deprecated)->requestMatchers
    로 변경하였음.

--- src/main/java/kr/semanticker/projectboard/config/SecurityConfig.java
+++ src/main/java/kr/semanticker/projectboard/config/SecurityConfig.java
@@ -1,25 +1,46 @@
 package kr.semanticker.projectboard.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-      // 공식 문서 코드
-       /* http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());*/
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .authorizeHttpRequests(
+                        //auth -> auth.anyRequest().permitAll()
+                        auth -> auth.requestMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"
+                        ).permitAll().anyRequest().authenticated()
+                )
                 .formLogin().and()
                 .build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // static resources => css, js
+        return (web) -> web.ignoring().requestMatchers( PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::form)
+                .map(princial)
+    }*/
+
 
 }
