2eb0eadb431c1d7be640d9e95883edc82238a748
semanticker <semanticker@gmail.com>
2023-07-10 21:32:38 +0900

    #43-SpringSecurity 설정 수정
    securityFilterChain에서 permitAll 처리하도록 수정 (Spring Security 권장사항)

--- src/main/java/kr/semanticker/projectboard/config/SecurityConfig.java
+++ src/main/java/kr/semanticker/projectboard/config/SecurityConfig.java
@@ -1,46 +1,65 @@
 package kr.semanticker.projectboard.config;
 
+import kr.semanticker.projectboard.dto.UserAccountDto;
+import kr.semanticker.projectboard.dto.security.BoardPrincipal;
+import kr.semanticker.projectboard.repository.UserAccountRepository;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(
                         //auth -> auth.anyRequest().permitAll()
-                        auth -> auth.requestMatchers(
-                                HttpMethod.GET,
-                                "/",
-                                "/articles",
-                                "/articles/search-hashtag"
-                        ).permitAll().anyRequest().authenticated()
+                        auth -> auth
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .requestMatchers(
+                                    HttpMethod.GET,
+                                    "/",
+                                    "/articles",
+                                    "/articles/search-hashtag"
+                                ).permitAll().anyRequest().authenticated()
                 )
-                .formLogin().and()
+                .formLogin()
+                    .and()
+                .logout()
+                    .logoutSuccessUrl("/")
+                    .and()
                 .build();
     }
 
+    /* securityFilterChain 에서 permitAll 처리
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         // static resources => css, js
         return (web) -> web.ignoring().requestMatchers( PathRequest.toStaticResources().atCommonLocations());
     }
+    */
 
-    /*@Bean
+    @Bean
     public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
         return username -> userAccountRepository
                 .findById(username)
-                .map(UserAccountDto::form)
-                .map(princial)
-    }*/
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(()-> new UsernameNotFoundException("유저를 찾을 수 없습니다. - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 
 }
