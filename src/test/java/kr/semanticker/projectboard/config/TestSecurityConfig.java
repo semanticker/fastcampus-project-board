045799c777a1d4c0421979fdfc398d356db3d746
semanticker <semanticker@gmail.com>
2023-07-11 22:42:51 +0900

    #43-테스트용 SecurityConfig 추가
    인증을 마치 수행한것처럼 효과를 낸다.

---
+++ src/test/java/kr/semanticker/projectboard/config/TestSecurityConfig.java
@@ -0,0 +1,32 @@
+package kr.semanticker.projectboard.config;
+
+import kr.semanticker.projectboard.domain.UserAccount;
+import kr.semanticker.projectboard.repository.UserAccountRepository;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@Import(SecurityConfig.class)
+public class TestSecurityConfig {
+    @MockBean private UserAccountRepository userAccountRepository;
+
+    @BeforeTestMethod
+    public void securitySetUp() {
+        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(
+                UserAccount.of(
+                    "test",
+                    "pw",
+                    "test@email.com",
+                    "test-nick",
+                    "test-memo"
+                )
+        ));
+    }
+}
