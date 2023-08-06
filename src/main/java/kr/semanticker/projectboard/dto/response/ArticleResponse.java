8a8b6e5fafc17e3d5dc2bcd132328d88fd6faada
semanticker <semanticker@gmail.com>
2023-08-06 21:56:19 +0900

    #56-Article엔티티의 String 타입의 Hashtag를 Set 타입으로 변경2
    관련된 DTO 등 파일 수정

--- src/main/java/kr/semanticker/projectboard/dto/response/ArticleResponse.java
+++ src/main/java/kr/semanticker/projectboard/dto/response/ArticleResponse.java
@@ -2,21 +2,24 @@ package kr.semanticker.projectboard.dto.response;
 
 
 import kr.semanticker.projectboard.dto.ArticleDto;
+import kr.semanticker.projectboard.dto.HashtagDto;
 
 import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public record ArticleResponse(
         Long id,
         String title,
         String content,
-        String hashtag,
+        Set<String> hashtags,
         LocalDateTime createdAt,
         String email,
         String nickname
 ) {
 
-    public static ArticleResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname) {
-        return new ArticleResponse(id, title, content, hashtag, createdAt, email, nickname);
+    public static ArticleResponse of(Long id, String title, String content, Set<String> hashtags, LocalDateTime createdAt, String email, String nickname) {
+        return new ArticleResponse(id, title, content, hashtags, createdAt, email, nickname);
     }
 
     public static ArticleResponse from(ArticleDto dto) {
@@ -29,7 +32,9 @@ public record ArticleResponse(
                 dto.id(),
                 dto.title(),
                 dto.content(),
-                dto.hashtag(),
+                dto.hashtagDtos().stream()
+                    .map(HashtagDto::hashtagName)
+                    .collect(Collectors.toUnmodifiableSet()),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname
--- src/test/java/kr/semanticker/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/kr/semanticker/projectboard/controller/ArticleControllerTest.java
@@ -255,7 +255,7 @@ class ArticleControllerTest {
     @Test
     void givenNewArticleInfo_whenRequesting_thenSavesNewArticle() throws Exception {
         // Given
-        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content", "#new");
+        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content");
         willDoNothing().given(articleService).saveArticle(any(ArticleDto.class));
 
         // When & Then
@@ -309,7 +309,7 @@ class ArticleControllerTest {
     void givenUpdatedArticleInfo_whenRequesting_thenUpdatesNewArticle() throws Exception {
         // Given
         long articleId = 1L;
-        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content", "#new");
+        ArticleRequest articleRequest = ArticleRequest.of("new title", "new content");
         willDoNothing().given(articleService).updateArticle(eq(articleId), any(ArticleDto.class));
 
         // When & Then
